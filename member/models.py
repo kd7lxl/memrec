56955719f0e155c234f0497363587b9474e88444
Tom Hayward <esarfl+github@gmail.com>
2012-11-26 11:26:19 -0800

    member: PEP8 fixes

--- member/models.py
+++ member/models.py
@@ -21,29 +21,30 @@ class Person(models.Model):
     """
     last_name = models.CharField(max_length=30)
     first_name = models.CharField(max_length=30)
-    dem_number = models.PositiveIntegerField(null=True, blank=True)
-    dob = models.DateField(null=True,blank=True)
+    dem_number = models.PositiveIntegerField(blank=True)
+    dob = models.DateField(null=True, blank=True)
     join_date = models.DateField(null=True, blank=True)
     person_type = models.CharField(max_length=20, choices=(
         ('contact', 'contact'),
         ('recruit', 'recruit'),
         ('member', 'member'),
+        ('past member', 'past member'),
     ))
     emergency_contact_1 = models.ForeignKey('self', blank=True, null=True,
         related_name='emergency_contact1_for')
     emergency_contact_2 = models.ForeignKey('self', blank=True, null=True,
         related_name='emergency_contact2_for')
     date_added = models.DateField(auto_now_add=True)
-    
+
     def __unicode__(self):
         return u'%s %s' % (self.first_name, self.last_name)
-    
+
     def age(self, today=date.today()):
         born = self.dob
-        try: # raised when birth date is February 29 and the current year is not a leap year
+        try:  # raised when birth date is February 29 and the current year is not a leap year
             birthday = born.replace(year=today.year)
         except:
-            birthday = born.replace(year=today.year, day=born.day-1)
+            birthday = born.replace(year=today.year, day=born.day - 1)
         if birthday > today:
             yearsold = today.year - born.year - 1
         else:
@@ -58,13 +59,13 @@ class Person(models.Model):
             return '%s yr' % (yearsold)
         else:
             return '%s yrs' % (yearsold)
-    
+
     def time_in_unit(self, today=date.today()):
         born = self.join_date
-        try: # raised when birth date is February 29 and the current year is not a leap year
+        try:  # raised when birth date is February 29 and the current year is not a leap year
             birthday = born.replace(year=today.year)
         except:
-            birthday = born.replace(year=today.year, day=born.day-1)
+            birthday = born.replace(year=today.year, day=(born.day - 1))
         if birthday > today:
             yearsold = today.year - born.year - 1
         else:
@@ -79,27 +80,30 @@ class Person(models.Model):
             return '%s yr' % (yearsold)
         else:
             return '%s yrs' % (yearsold)
-    
+
     class Meta:
         ordering = ['last_name', 'first_name']
-    
+
+
 class MembershipFeePayment(models.Model):
     person = models.ForeignKey(Person)
     payment_date = models.DateField()
     payment_amount = models.DecimalField(max_digits=5, decimal_places=2)
-    
+
     def __unicode__(self):
         return u'%s paid $%02.f on %s' % (self.person, self.payment_amount, self.payment_date)
+
 
 class EmailAddress(models.Model):
     person = models.ForeignKey(Person)
     email_address = models.EmailField()
-    
+
     def __unicode__(self):
         return u'%s' % (self.email_address)
-    
+
     class Meta:
         verbose_name_plural = 'email addresses'
+
 
 class Address(models.Model):
     person = models.ForeignKey(Person)
@@ -108,19 +112,29 @@ class Address(models.Model):
     city = models.CharField(max_length=30)
     state = models.CharField(max_length=2)
     postal_code = models.CharField(max_length=9)
-    
+
+    def __unicode__(self):
+        return ', '.join([
+            self.address1,
+            self.address2 or '',
+            self.city,
+            self.state,
+            self.postal_code,
+        ])
     class Meta:
         verbose_name_plural = 'addresses'
+
 
 class ServiceProvider(models.Model):
     name = models.CharField(max_length=30)
     sms_email_hostname = models.CharField(max_length=60, validators=[validate_hostname])
-    
+
     def __unicode__(self):
         return u'%s' % (self.name)
-    
+
     class Meta:
         ordering = ['name']
+
 
 class Phone(models.Model):
     person = models.ForeignKey(Person)
@@ -135,18 +149,18 @@ class Phone(models.Model):
     ))
     service_provider = models.ForeignKey(ServiceProvider, null=True, blank=True)
     sms_enabled = models.BooleanField(default=False)
-    
+
     def __unicode__(self):
         return u'(%s) %s-%s' % (
             self.phone_number[0:3],
             self.phone_number[3:6],
             self.phone_number[6:10],
         )
-    
+
     def sms_email_address(self):
         '''
         Generates an sms email address from number and provider.
-        
+
         >>> person = Person(last_name="Smith", first_name="John")
         >>> person.save()
         >>> verizon = ServiceProvider.objects.get(name="Verizon")
@@ -159,6 +173,6 @@ class Phone(models.Model):
         if self.service_provider is not None \
         and self.sms_enabled is True:
             return '%s@%s' % (self.phone_number, self.service_provider.sms_email_hostname)
-    
+
     class Meta:
         verbose_name = 'phone number'
