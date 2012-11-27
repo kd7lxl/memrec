97f91a53178ddb3a12ab3462c3c6b48346f23f7f
Tom Hayward <esarfl+github@gmail.com>
2012-11-26 17:14:45 -0800

    member: add Dropped filter

--- member/admin.py
+++ member/admin.py
@@ -1,5 +1,32 @@
 from models import *
 from django.contrib import admin
+from django.contrib.admin.filters import SimpleListFilter
+
+
+class NullFilterSpec(SimpleListFilter):
+    title = u''
+    parameter_name = u''
+
+    def lookups(self, request, model_admin):
+        return (
+            ('1', self.title),
+            ('0', 'Not %s' % self.title),
+        )
+
+    def queryset(self, request, queryset):
+        kwargs = {
+            str('%s__isnull' % self.parameter_name): True,
+        }
+        if self.value() == '0':
+            return queryset.filter(**kwargs)
+        if self.value() == '1':
+            return queryset.exclude(**kwargs)
+        return queryset
+
+
+class DroppedNullFilterSpec(NullFilterSpec):
+    title = u'Dropped'
+    parameter_name = u'drop_date'
 
 
 class PersonAdmin(admin.ModelAdmin):
@@ -12,6 +39,7 @@ class PersonAdmin(admin.ModelAdmin):
     )
     list_filter = (
         'join_date',
+        DroppedNullFilterSpec,
     )
     search_fields = (
         'last_name',
